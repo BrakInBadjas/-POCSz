147c56086434986700013fdbb2779f00d4445712
chrisje <chriswitteveen2@hotmail.com>
2016-10-31 00:18:42 +0100

    re-organized code and removed code used for debugging

--- db.py
+++ db.py
@@ -1,143 +1,87 @@
 import MySQLdb
-import time
 
-#Open connection to the database
+#Open connection to Database and start cursor. We use the cursor to execute queries.
 db = MySQLdb.connect(host="sql7.freesqldatabase.com",    	# your host, usually localhost
                      user="sql7142368",         			# your username
                      passwd="y65TanAMCg",  					# your password
                      db="sql7142368")      				  	# name of the data base					 
 cur = db.cursor()
 
-#Get a public_key from the Door Table selected on id
-#returns a public_key belonging to the door_id
-def getDoorPublicKey(door_id):
-	query = 'SELECT public_key FROM Door WHERE id=' + str(door_id) + ';'
-	cur.execute(query)
-	return cur.fetchone()[0]
-	
-#Add a new Door to the Door Table
-def addNewDoor(door_id, public_key):
-	query = 'INSERT INTO Door VALUES (' + str(door_id) + ',\'' + public_key + '\');'
-	cur.execute(query)
-	db.commit()	
-	
-#Get a Role from the Role Table selected on id
-#returns the role_name belonging to the role_id
-def getRole(role_id):
-	query = 'SELECT name FROM Role WHERE id=' + str(role_id) + ';'
-	cur.execute(query)
-	return cur.fetchone()[0]
-	
-#Add a new Role to the Role Table
+#Function to add new entries to the Database
+#Add new Role to the Database
 def addNewRole(role_id, role_name):
 	query = 'INSERT INTO Role VALUES (' + str(role_id) + ',\'' + role_name + '\');'
 	cur.execute(query)
 	db.commit()	
 	
-#Get a Person from the Person Table selected on id
-#returns a 4-tuple containing (person_id, key_uid, person_name, role_id) belonging to the id
-def getPerson(user_id):
-	query = 'SELECT * FROM Person WHERE key_uid=' + str(user_id) + ';'
-	cur.execute(query)
-	return cur.fetchone()
-	
-#Get a Person from the Person Table selected on key_uid
-#returns a 4-tuple containing (person_id, key_uid, person_name, role_id) belonging to the key_uid
-def getPersonByUID(key_uid):
-	query = 'SELECT * FROM Person WHERE key_uid=\'' + str(key_uid) + '\';'
-	cur.execute(query)
-	return cur.fetchone()
-	
-#Add a new Person to the Person Table
+#Add a new Person to the Database
 def addNewPerson(person_id, key_uid, name, role_id):
 	query = 'INSERT INTO Person VALUES (' + str(person_id) + ',\'' + key_uid + '\',\'' + name + '\',' + str(role_id) + ');'
 	cur.execute(query)
 	db.commit()	
 	
-#Get a all door_id's to which a person has access
-#returns a list of door_id's
-def getPersonDoor(user_id):
-	query = 'SELECT door_id FROM Person_Door WHERE person_id=' + str(user_id) + ';'
+#Add a new Door to the Database
+def addNewDoor(door_id, public_key):
+	query = 'INSERT INTO Door VALUES (' + str(door_id) + ',\'' + public_key + '\');'
 	cur.execute(query)
-	return [list(a) for a in cur.fetchall()]
+	db.commit()	
 	
-#Add a new Combination of person_id, door_id to the Person_Door Table
+#Add a new Permission, to open a Door, for a Person to the Database
 def addNewPersonDoor(user_id, door_id):
 	query = 'INSERT INTO Person_Door VALUES (' + str(user_id) + ',' + str(door_id) + ');'
 	cur.execute(query)
 	db.commit()	
 	
-#Get a all door_id's to which a role has access
-#returns a list of door_id's
+#Add a new Permission, to open a Door, for a Role to the Database
+def addNewRoleDoor(role_id, door_id):
+	query = 'INSERT INTO Role_Door VALUES (' + str(role_id) + ',' + str(door_id) + ');'
+	cur.execute(query)
+	db.commit()	
+
+#Functions to get data from the Database	
+#Returns the role belonging to the role_id
+def getRole(role_id):
+	query = 'SELECT name FROM Role WHERE id=' + str(role_id) + ';'
+	cur.execute(query)
+	return cur.fetchone()[0]
+	
+#Returns the public_key belonging to the door_id
+def getDoorPublicKey(door_id):
+	query = 'SELECT public_key FROM Door WHERE id=' + str(door_id) + ';'
+	cur.execute(query)
+	return cur.fetchone()[0]
+
+#Return a 4-tuple of (ID, Key UID, Name, Role ID) belonging to the user_id
+def getPerson(user_id):
+	query = 'SELECT * FROM Person WHERE key_uid=' + str(user_id) + ';'
+	cur.execute(query)
+	return cur.fetchone()
+
+#Return a 4-tuple of (ID, Key UID, Name, Role ID) belonging to the key_uid	
+def getPersonByUID(key_uid):
+	query = 'SELECT * FROM Person WHERE key_uid=\'' + str(key_uid) + '\';'
+	cur.execute(query)
+	return cur.fetchone()
+	
+#Returns a list of all doors a person has access to (Access by group not included)
+def getPersonDoor(user_id):
+	query = 'SELECT door_id FROM Person_Door WHERE person_id=' + str(user_id) + ';'
+	cur.execute(query)
+	return [list(a) for a in cur.fetchall()]
+
+#Returns a list of all doors a role has access to
 def getRoleDoor(role_id):
 	query = 'SELECT door_id FROM Role_Door WHERE role_id=' + str(role_id) + ';'
 	cur.execute(query)
 	return [list(a) for a in cur.fetchall()]
 	
-#Add a new Combination of role_id, door_id to the Role_Door Table
-def addNewRoleDoor(role_id, door_id):
-	query = 'INSERT INTO Role_Door VALUES (' + str(role_id) + ',' + str(door_id) + ');'
-	cur.execute(query)
-	db.commit()	
+#Checks if a give Key UID has access to a certain door
+def hasPermission(key_uid, door_id):
+	person = getPersonByUID(key_uid)
 	
-#Some test commands	
-
-#To use the add function, first be sure there is no entry with that id in the table you want to add something to
-#	addNewDoor(5, 'asasdfpoirewmsnd')
-#	addNewRole(1, 'Tester')
-#	addNewPerson(0, 'DC 7C 98 1E', 'Chris Witteveen', 1)
-#	addNewPersonDoor(0, 0)
-#	addNewRoleDoor(1, 1)
-
-#Specify for which user we are going to check to which doors he has acces
-person_id = 0
-person = getPerson(person_id)
-
-#Check acces, first by person_id then by role_id
-print person[2] + " has access to the following doors:"
-for row in getPersonDoor(person_id):
-	print " - Door " + str(row[0]) + " with public key: " + getDoorPublicKey(row[0])
-print "His role: " + getRole(person[3]) + ", grants him access to the following doors:"
-for row in getRoleDoor(person[3]):
-	print " - Door " + str(row[0]) + " with public key: " + getDoorPublicKey(row[0])
-print "His Key UID is: " + person[1]
-
-#For readability
-print ""
-print ""
-
-startt = time.time()
-
-#Specify which door to open
-doorToOpen = 1L
-
-#Check who tried to enter the door
-person = getPersonByUID("DC 7C 98 1E")
-print "A card with UID: DC 7C 98 1E, has tried to open door " + str(doorToOpen)
-print "This card belongs to " + person[2] + " with ID: " + str(person[0]) + " and Role ID: " + str(person[3])
-
-#Check if the user has access to the door
-if ([doorToOpen]) in getPersonDoor(person[0]):
-	print "He has acces to this door!"
-elif ([doorToOpen]) in getRoleDoor(person[3]):
-	print "His role has granted him access!"
-else:
-	print person[2] + " has no access to this door!"
-	
-#Check how long it took to check who tried to gain access and if he has access
-endt = time.time()
-print "it took " + str(endt - startt) + " seconds to check if the card had access!"	
-
-#Example output:
-#  	Chris Witteveen has access to the following doors:
-#  	 - Door 0 with public key: asjfde942e9fdsffsdfsdfsdfsdfsdfsddsf
-#  	His role: Tester, grants him access to the following doors:
-#  	 - Door 0 with public key: asjfde942e9fdsffsdfsdfsdfsdfsdfsddsf
-#  	 - Door 1 with public key: asjfde942e9fasdfsddsf
-#  	His Key UID is: DC 7C 98 1E
-#  	
-#  	
-#  	A card with UID: DC 7C 98 1E, has tried to open door 5
-#  	This card belongs to Chris Witteveen with ID: 0 and Role ID: 1
-#  	Chris Witteveen has no access to this door!
-#  	it took 0.0780000686646 seconds to check if the card had access!
+	if ([door_id] in getPersonDoor(person[0])):
+		return True
+	elif ([door_id] in getRoleDoor(person[3])):
+		return True
+	else:
+		return False
