583c7a8220b6bc7f5f8882de746c2ce4556e8baf
Chris Witteveen <Chriswitteveen2@hotmail.com>
2016-11-04 11:55:40 +0100

    Removed Gui from setup

--- setup.bat
+++ setup.bat
@@ -7,13 +7,6 @@ echo Installing pyserial
 pip install pyserial
 
 echo.
-echo If you get the error: 'Module winrandom not found'
-echo Go to file: (...\Lib\site-packages\Crypto\Random\OSRNG\nt.py) Inside your python folder
-echo Change: "import winrandom"
-echo to: "from . import winrandom" 
-echo Then restart this bat file
-
-echo.
 echo Installing PyMySQL
 pip install pymysql
 
@@ -24,10 +17,5 @@ pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
 echo.
 echo Installing Kivy
 pip install kivy
-
-echo.
-echo.
-echo Launching GUI
-python Python/dbGui.py
 
 PAUSE
